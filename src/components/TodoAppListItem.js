1607fbb602f528a6f2098ddded094ef00f05d814
Bengi <bengihazal@gmail.com>
2020-05-02 21:57:07 +0300

    Update TodoAppListItem.js

--- src/components/TodoAppListItem.js
+++ src/components/TodoAppListItem.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {connect} from "react-redux";
 import {toggleTodo} from "../redux/actions";
+import "./style.css";
 
 const TodoAppListItem = ({content, id, completed, toggleTodo}) => {
     return (
-        <div style={{
+        <div> 
+           <button className= "btn"  style={{
             textDecoration: completed ? "line-through" : "initial"
-        }} onClick={() => toggleTodo(id)}>
+        }}  onClick={() => toggleTodo(id)} >
+           -
+           </button>
             {content}
         </div>
+        
+        
     );
 };
 
